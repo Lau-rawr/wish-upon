635f4f183e5b1a2c6066e8576bcd76e17f728fe7
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-12-12 21:58:42 -0800

    Update ImageGallery.js
    updating local host

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect, useContext, useRef } from "react";
 import UserContext from "../context/UserContext";
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
-import ImageCard from '../components/imageCard';
-import { ReactSVG } from 'react-svg';
+
 
 export default function ImageGallery() {
     const [allImages, addImage] = useState([{}]);
-    // const images = useRef([{}]);
     const { userData } = useContext(UserContext);
     const history = useHistory();
 
@@ -16,7 +14,6 @@ export default function ImageGallery() {
             const response = await axios.get(
                 "http://localhost:5000/image/all",
             );
-            // images.current = response.data;
             addImage(response.data);
             console.log(allImages);
         }
@@ -25,12 +22,11 @@ export default function ImageGallery() {
 
     const colorImage = async (image) => {
         if (!userData.token) {
-            history.push("/")
+            history.push("/signIn")
             return
         }
 
         let newImage = { imageId: image._id, component: image.component };
-        //console.log(newImage);
 
         const response = await axios.post(
             "http://localhost:5000/user_images/add",
@@ -42,20 +38,27 @@ export default function ImageGallery() {
     }
 
     return (
-        <div>
+        <>
             <h1>Choose an image to color!</h1>
-            {allImages.map((image, i) => {
-                console.log(image)
-                return (
-                    <div className="image-card" key={i}>
-                        <h4>{image.title}</h4>
-                        <ReactSVG src={image.svg_url} />
-                        <button onClick={() => colorImage(image)}>Color Me!</button>
-                    </div>
+            <div class="image-gallery">
+                {allImages.map((image, i) => {
+                    return (
+                        <>
+                            <div class="image-card" id="gallery" onClick={() => colorImage(image)}>
+                                <h4>{image.title}</h4>
+                                {image.component !== undefined ?
+                                    <>
+                                        <img alt="disney" className="new-image" src={image.svg_url} />
+                                       
+                                    </>
+                                    : <>Loading</>}
+                            </div>
+                        </>
+                    )
+                }
                 )
-            })}
-            {/* <ReactSVG src="https://res.cloudinary.com/drqctijmr/image/upload/v1606887811/22_Stay_weird_rsqaja.svg" /> */}
-            {/* </div>  */}
-        </div>
+                }
+            </div>
+        </>
     )
 }
