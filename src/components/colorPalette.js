3a0b19bb3aa555b1779a28724041cfb6ae59223d
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-12-12 21:57:44 -0800

    Update colorPalette.js
    updating colors in the palette

--- src/components/colorPalette.js
+++ src/components/colorPalette.js
@@ -1,27 +1,25 @@
-import React, {useEffect, useContext} from "react";
+import React from "react";
+// import { PopoverPicker } from "../components/PopoverPicker";
 
 export default function ColorPalette(props) {
-    
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'turquoise', 'purple', 'pink', 'deeppink',	
-     '#66FF66','#FF9966','#50BFE6','#ac1db8','#0bf9ea','#ff08e8']
 
-    return(
-        <>
-        
-        <img id="backgroundPalette"  />
-       
-        {/* <img src={require('./images/mickeyPalette.png')} /> */}
-        <div className="color-palette" >
-            {colors.map(color => { 
-                const activeClass = props.currentColor === color ? 'color-swatch-active' : '';
+    const colors = ['#C53151', '#FF355E', '#FF00CC', '#FF6037', '#FFAE42', '#FFFF66', '#93DFB8', '#66FF66', '#319177', '#00CCCC', '#59baff', '#00468C', '#652DC1', '#FFCBA4', '#C8C8CD', '#4a1200', '#ffffff', '#000000']
+
+    return (
+        <div className="color-palette">
+            {colors.map(color => {
+                // looping through all of the colors and highlights a color in palette if selected 
+                const activeClass = props.currentColor === color ?
+                    'color-swatch-active' :
+                    '';
                 return (
-                    <div onClick={() => {props.changeColor(color)}}>
-                        <div className={`color-swatch ${activeClass}`}  style={{backgroundColor: color}}></div>
+                    // color is changed 
+                    <div onClick={() => { props.changeColor(color) }}>
+                        <div className={`color-swatch ${activeClass}`} style={{ backgroundColor: color }}></div>
                     </div>
                 )
             })}
+         
         </div>
-        </>
-        
     )
 }
