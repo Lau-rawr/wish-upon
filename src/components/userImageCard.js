f7645ab9adf673a6f32e64c739f3daa57bce89da
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-12-12 21:44:27 -0800

    Update userImageCard.js
    updating changes

--- src/components/userImageCard.js
+++ src/components/userImageCard.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom';
 import UserContext from "../context/UserContext";
 import axios from 'axios';
 import swal from 'sweetalert';
-import Stitch from "../components/images/stitch.js";
-import Minnie from "../components/images/minnie.js";
+import ColoredInImage from "./images/coloredInImage";
+
 export default function UserImageCard(props) {
     const [image, setImage] = useState({});
-    var ColorImage = useRef();
     const { userData } = useContext(UserContext);
     const history = useHistory();
 
     useEffect(() => {
         setImage(props.userImage)
     }, [props])
+
+    const downloadImage = () => {
+        history.push(`/imageDownload/${image._id}`)
+    }
 
     const deleteImage = async (id) => {
         if (!userData.token) {
@@ -26,26 +28,51 @@ export default function UserImageCard(props) {
             text: "This cannot be reversed.",
             buttons: true
         })
-        .then( async(willDelete) => {
-            if (willDelete) {
-                await axios.delete(
-                    `/user_images/${id}`,
-                    { headers: { "x-auth-token": userData.token } },
-                );  
-            }
-        });
+            .then(async (toDelete) => {
+                if (toDelete) {
+                    await axios.delete(
+                        `http://localhost:5000/user_images/${id}`,
+                        { headers: { "x-auth-token": userData.token } },
+                    );
+                }
+            });
+    }
+
+    const editImage = () => {
+        history.push(`/coloringpage/${image._id}`)
+    }
+
+    const showcaseImage = async (id) => {
+        image.public = image.public === true ? image.public = false : image.public = true
+
+        setImage({
+            ...image
+        })
+        try {
+            const response = await axios.post(
+                `http://localhost:5000/user_images/add/${id}`,
+                image,
+                { headers: { "x-auth-token": userData.token } },
+            );
+            console.log(response);
+        } catch (err) {
+            console.log("Unable to save");
+        }
     }
 
     return (
         <div className="image-card">
-            {image.component === "stitch" ?
-            <div>
-                <Stitch component={image.component} onFill={()=>{}} fillColors={image.fill_colors} />
-                <Link to={"/coloringpage/"+image._id}>Edit</Link> | 
-                <button onClick={() => deleteImage(image._id)}>Delete</button> |
-                <Link to={"/imageDownload/"+image._id}>Download</Link>
-            </div> :
-            <>Hello</>
+            {image.component !== undefined ?
+                <>
+                    <ColoredInImage component={image.component} size={props.size} onFill={() => { }} fillColors={image.fill_colors} />
+                    <div class="btn-group flex-wrap" id="user">
+                        <button class="download" onClick={() => downloadImage()}>Download</button>
+                        <button class="delete" onClick={() => deleteImage(image._id)}>Delete</button>
+                        <button class="color-me" onClick={() => editImage()}>Edit</button>
+                        <button onClick={() => showcaseImage(image._id)} class={image.public ? "showcasing" : "unpublished"}>{image.public ? "Showcasing" : "Unpublished"}</button>
+                    </div>
+                </>
+                : <>Loading</>
             }
         </div>
     )
