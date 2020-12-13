4f285ba819b76819139eec5cb988b14cdc2cd780
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-12-12 21:48:06 -0800

    Update showcaseImageCard.js
    updating

--- src/components/showcaseImageCard.js
+++ src/components/showcaseImageCard.js
@@ -1,13 +1,7 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
-import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import swal from 'sweetalert';
-import Stitch from "../components/images/stitch.js";
-import Minnie from "../components/images/minnie.js";
+import React, { useState, useEffect } from "react";
+import ColoredInImage from "./images/coloredInImage";
 export default function ShowcaseImageCard(props) {
   const [image, setImage] = useState({});
-  const history = useHistory();
 
   useEffect(() => {
     setImage(props.userImage)
@@ -15,11 +9,9 @@ export default function ShowcaseImageCard(props) {
 
   return (
     <div className="image-card">
-      {image.component === "stitch" ?
-        <div>
-          <Stitch component={image.component} onFill={()=>{}} fillColors={image.fill_colors} />
-        </div> :
-        <>Start coloring so your works can be here!</>
+      {image.component !== undefined ?
+        <ColoredInImage component={image.component} size={props.size} onFill={() => { }} fillColors={image.fill_colors} />
+        : <>Loading</>
       }
     </div>
   )
