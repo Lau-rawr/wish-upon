fbf32e45d1fbefefbd23326e7aec72c0ae154fe1
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-12-12 21:50:39 -0800

    Update navbar.component.js
    updating

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,6 +1,5 @@
 import React, { Component }  from 'react';
 import { Link } from 'react-router-dom';
-import { withRouter } from "react-router";
 import AuthOptions from './AuthOptions';
 
 export default class Navbar extends Component {
@@ -18,20 +17,10 @@ export default class Navbar extends Component {
     render() {
         const show = (this.state.menu) ? "show" : "" ;
     return(
-        // <nav class="navbar navbar-expand-lg">
-        //     <Link to="/home" class="navbar-brand">Wish Upon a Star</Link>
-        //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        //         <span class="navbar-toggler-icon"></span>
-        //     </button>
-        //     <div class="collpase navbar-collapse" id="navbarNav">
-        //         <ul class="navbar-nav nav-pills ml-auto">
-        //             <AuthOptions />
-        //         </ul>
-        //     </div>
-        // </nav>
+        
 
         <nav class = "navbar navbar-expand-lg" >
-            <Link to="/home" class="navbar-brand">Wish Upon a Star</Link>
+            <Link to="/" class="navbar-brand">Wish Upon a Star</Link>
             <button class="navbar-toggler custom-toggler" onClick={() => this.toggleMenu()} type="button" data-toggle="collapse" data-target=".navbar-nav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
